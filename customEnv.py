df916a52a83cedfb33fbf63b52729b167b0ee71c
Mihai <rares.bors@ulb.be>
2023-05-06 01:57:47 +0200

    removed need for finding the player or enemies
    using external counters such as hp and gem count, we can theoretically tell if we illed an enemy or not, and so on

--- customEnv.py
+++ customEnv.py
@@ -5,7 +5,7 @@ import numpy as np
 import pyautogui
 import pygetwindow as gw
 
-from imageProcessing import extract_hp, find_player
+from imageProcessing import extract_info
 
 # -------PATHS---------
 dir = os.path.dirname(__file__)
@@ -20,7 +20,8 @@ class CustomDownwellEnvironment:
     def __init__(self):
         self.game_window = None
         self.actions = ['left', 'right', 'space']
-        self.prev_pos = None
+        self.prev_hp = None
+        self.prev_gems = 0
         self.platform_counter = 0
 
     def window_exists(self):
@@ -49,22 +50,9 @@ class CustomDownwellEnvironment:
             region=(self.game_window.left, self.game_window.top, self.game_window.width, self.game_window.height))
         game_screen = np.array(screenshot)
 
-        templates = [standing, jumping]  # Add more templates here
-        top_left, bottom_right = find_player(game_screen, templates)
-
-        if top_left and bottom_right:
-            cv.rectangle(game_screen, top_left, bottom_right, (0, 255, 0), 2)
-            player_pos = (top_left[1] + bottom_right[1]) // 2
-
-            if self.prev_pos and 5 > abs(player_pos - self.prev_pos) > 0:
-                self.platform_counter += 1
-            else:
-                self.platform_counter = 0
-
-            self.prev_pos = player_pos
-
-        cv.imshow('Game Screen with Hitboxes', game_screen)
+        cv.imshow('Game Screen', game_screen)
         cv.waitKey(1)
+
         return game_screen
 
     def step(self, action):
@@ -87,37 +75,34 @@ class CustomDownwellEnvironment:
         return next_state, reward, done
 
     def game_info(self, screen):
-        hp = extract_hp(screen)
-        return hp
+        hp, gems = extract_info(screen)
+        return hp, gems
 
     def calculate(self, screen):
-        hp = self.game_info(screen)
+        hp, gems = self.game_info(screen)
         reward = 0
         done = False
 
         # Exploration bonus
         reward += 1
 
-        #enemies = detect_enemies(screen)
-        #in_shop_bonus_area = detect_areas(screen)
-
+        # Reward for moving down
         if self.platform_counter > 10:
             reward -= 5
 
-        #if enemies:
-        #    for enemy in enemies:
-        #        distance = distance_enemy(screen, enemy)
-        #        reward += distance
+        # Reward for collecting gems
+        if self.prev_gems is not None and gems > self.prev_gems:
+            reward += 10 * (gems - self.prev_gems)
+        self.prev_gems = gems
 
-        reward += hp * 10
-
-        #if in_shop_bonus_area:
-        #    reward -= 1000
-
-        if reward < -250:
-            done = True
+        # Reward for HP change
+        if self.prev_hp is not None:
+            hp_change = hp - self.prev_hp
+            reward += hp_change * 10
+        self.prev_hp = hp
 
         if hp <= 0:
             done = True
+            reward = -1000
 
         return reward, done
--- imageProcessing.py
+++ imageProcessing.py
@@ -2,12 +2,18 @@ import pytesseract
 import cv2 as cv
 
 # -------PATHS---------
-path = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-pytesseract.pytesseract.tesseract_cmd = path
+#path = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+#pytesseract.pytesseract.tesseract_cmd = path
 # ---------------------
 
+def extract_info(screen):
+    hp = extract_hp(screen)
+    gems = extract_gems(screen)
+
+    return hp, gems
+
 def extract_hp(screen):
-    vals = {'44': 4, '34': 3, '5': 2, '14': 1}
+    hp_vals = {'44': 4, '34': 3, '5': 2, '14': 1}
 
     hp_area = screen[49:67, 30:194]
     hp_area = cv.cvtColor(hp_area, cv.COLOR_BGR2GRAY)
@@ -17,29 +23,25 @@ def extract_hp(screen):
     hp = pytesseract.image_to_string(hp_area, config='--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789')
     hp = hp.replace(' ', '').replace('\n', '')
 
-    if hp in vals:
-        hp = vals[hp]
+    if hp in hp_vals:
+        hp = hp_vals[hp]
     else:
         hp = 0
 
     return hp
 
-def find_player(screen, templates):
-    screen_gray = cv.cvtColor(screen, cv.COLOR_BGR2GRAY)
-    max_val = -1
-    max_loc = None
-    best_template = None
+def extract_gems(screen):
+    gems_area = screen[49:67, 225:300]
+    gems_area = cv.cvtColor(gems_area, cv.COLOR_BGR2GRAY)
+    gems_area = cv.threshold(gems_area, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)[1]
+    gems_area = cv.resize(gems_area, (0, 0), fx=2, fy=2)
 
-    for template in templates:
-        result = cv.matchTemplate(screen_gray, template, cv.TM_CCOEFF_NORMED)
-        min_val, max_val_current, _, max_loc_current = cv.minMaxLoc(result)
+    gems = pytesseract.image_to_string(gems_area, config='--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789')
+    gems = gems.replace(' ', '').replace('\n', '')
 
-        if max_val_current > max_val:
-            max_val = max_val_current
-            max_loc = max_loc_current
-            best_template = template
+    try:
+        gems = int(gems)
+    except ValueError:
+        gems = 0
 
-    top_left = max_loc
-    bottom_right = (top_left[0] + best_template.shape[1], top_left[1] + best_template.shape[0])
-
-    return top_left, bottom_right
+    return gems
